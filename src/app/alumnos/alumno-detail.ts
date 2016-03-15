0cf072b7191f048e11de1c7b44ae774f705187c5
Maes95 <michel.maes95@gmail.com>
2016-03-15 16:27:28 +0100

    Refactor Dashboard

--- src/app/alumnos/alumno-detail.ts
+++ src/app/alumnos/alumno-detail.ts
@@ -3,18 +3,7 @@ import {Alumno} from './alumno';
 
 @Component({
   selector: 'alumno-detail',
-  template: `
-    <div *ngIf="alumno">
-      <h2>Cosas de {{alumno.name}}</h2>
-      <div>
-        <input [(ngModel)]="alumno.tarea" placeholder="Ponle mas tarea" class="form-control" id="focusedInput1" type="text">
-      </div>
-      <div>
-        <p>Tarea de {{alumno.name}}</p>
-        <p> - {{alumno.tarea}}</p>
-      </div>
-    </div>
-  `,
+  templateUrl: 'app/alumnos/alumno-detail.html',
   inputs: ['alumno']
 })
 export class AlumnoDetailComponent {
---
+++ src/app/dashboard-entrenador/dashboard-entrenador.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from 'angular2/core';
+import {Router, Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import { Alumno } from '../alumnos/alumno'
+import { AlumnoDetailComponent } from '../alumnos/alumno-detail';
+import { AlumnoService } from '../alumnos/alumno-service';
+
+@Component({
+	selector: 'dashboard-entrenador',
+  styleUrls: ['app/inicio/inicio.css'],
+  templateUrl: 'app/dashboard-entrenador/dashboard-entrenador.html',
+	directives: [AlumnoDetailComponent],
+  providers: [AlumnoService]
+})
+
+export class DashboardEntrenador implements OnInit {
+
+  selectedAlumno: Alumno;
+	alumnos: Alumno[];
+
+  constructor(private _alumnoService: AlumnoService) { }
+
+  getAlumnos() {
+    this._alumnoService.getAlumnos().then(alumnos => this.alumnos = alumnos);
+  }
+
+  ngOnInit() {
+    this.getAlumnos();
+  }
+
+  onSelect(alumno: Alumno) { this.selectedAlumno = alumno; }
+}
