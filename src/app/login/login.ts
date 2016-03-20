729954cb875d12f239f2b1ea026f50bd94eb3d74
Sergio <sergioperezp1995@gmail.com>
2016-03-20 12:38:01 +0100

    Login y registros

---
+++ src/app/login/login.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'login',
+    templateUrl: 'app/login/login.html'
+})
+
+export class login {};
---
+++ src/app/registroAlumno/registroAlumno.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+	selector: 'registroAlumno',
+    templateUrl: 'app/registroAlumno/registroAlumno.html'
+})
+
+export class registroAlumno {};
