067c5638c12e09778275b4217b6b0ee0f96c1f28
Maes95 <m.maes@alumnos.urjc.es>
2016-03-24 17:57:11 +0100

    Dashboards enlazados

--- src/app/alumnos/alumno-service.ts
+++ src/app/alumnos/alumno-service.ts
@@ -2,9 +2,18 @@ import {Alumno} from './alumno';
 import {ALUMNOS} from '../recursos-estaticos';
 import {Injectable} from 'angular2/core';
 
+
 @Injectable()
 export class AlumnoService {
   getAlumnos() {
     return Promise.resolve(ALUMNOS);
+  }
+
+  getAlumno(id: number) {
+    let alumno = Promise.resolve(ALUMNOS).then(
+      alumnos => alumnos.filter(alumno => alumno.id === id)[0]
+    );
+    console.log(alumno);
+    return alumno;
   }
 }
--- src/app/dashboard-alumno/dashboard-alumno.ts
+++ src/app/dashboard-alumno/dashboard-alumno.ts
@@ -1,31 +1,32 @@
 import {Component, OnInit} from 'angular2/core';
-import {Router, Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {RouteParams} from 'angular2/router';
 import { Alumno } from '../alumnos/alumno'
-import { AlumnoDetailComponent } from '../alumnos/alumno-detail';
 import { AlumnoService } from '../alumnos/alumno-service';
 
 @Component({
 	selector: 'dashboard-alumno',
   styleUrls: ['app/inicio/inicio.css'],
   templateUrl: 'app/dashboard-alumno/dashboard-alumno.html',
-	directives: [AlumnoDetailComponent],
   providers: [AlumnoService]
 })
 
 export class DashboardAlumno implements OnInit {
 
-  selectedAlumno: Alumno;
-	alumnos: Alumno[];
+	alumno: Alumno;
 
-  constructor(private _alumnoService: AlumnoService) {}
+	constructor(
+		private _alumnoService: AlumnoService,
+		private _routeParams: RouteParams) {
+	}
 
-  getAlumnos() {
-    this._alumnoService.getAlumnos().then(alumnos => this.alumnos = alumnos);
-  }
+	getAlumno(){
+		let id = +this._routeParams.get('id');
+		console.log(id);
+		this._alumnoService.getAlumno(id)
+			.then(alumno => this.alumno = alumno);
+	}
 
-  ngOnInit() {
-    this.getAlumnos();
-  }
-
-  onSelect(alumno: Alumno) { this.selectedAlumno = alumno; }
+	ngOnInit() {
+		this.getAlumno();
+	}
 }
