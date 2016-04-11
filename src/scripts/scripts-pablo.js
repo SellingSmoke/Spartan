16cb44530b30b637841ee6d7c25af97a33a6e7fb
pabloFuente <pablofuenteperez@gmail.com>
2016-04-11 11:11:14 +0200

    Script externo incluido en index.html

---
+++ src/scripts/scripts-pablo.js
@@ -0,0 +1,8 @@
+function showDialog(){
+    $('.wrap, .glyphicon-plus').toggleClass('active');
+
+    $('#blurizable').toggleClass('blur-backgorund change-style-blur');
+    return false;
+  }
+
+  //window.onload = showDialog;
