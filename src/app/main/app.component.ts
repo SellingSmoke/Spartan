28d964bc8085c5f1045b415c89507ed5d75e46f0
Maes95 <m.maes@alumnos.urjc.es>
2016-04-17 16:56:11 +0200

    Gender pipe + New menu

--- src/app/main/app.component.ts
+++ src/app/main/app.component.ts
@@ -4,7 +4,6 @@ import {Router, Route, RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 // import {DashboardAlumno} from '../dashboard-alumno/dashboard-alumno';
 import {Inicio} from '../inicio/inicio';
 import {Perfil} from '../perfil/perfil';
-import {Mensajes} from '../mensajes/mensajes';
 import {LoggedInRouterOutlet} from '../autenticacion/router';
 import {Login} from '../login/login';
 import {RegistroEntrenador} from '../registroEntrenador/registroEntrenador';
@@ -23,9 +22,7 @@ import { Autenticacion } from '../autenticacion/autenticacion';
 
 @RouteConfig([
   new Route({ path: '/inicio', component: Inicio, name: 'Inicio', useAsDefault: true}),
-	// new Route({ path: '/alumno/:id', component: DashboardAlumno, name: 'DashboardAlumno'}),
   new Route({ path: '/perfil', component: Perfil, name: 'Perfil'}),
-  new Route({ path: '/mensajes', component: Mensajes, name: 'Mensajes'}),
   new Route({ path: '/login', component: Login, name: 'Login'}),
   new Route({ path: '/registroEntrenador', component: RegistroEntrenador, name: 'RegistroEntrenador'}),
   new Route({ path: '/registroAlumno', component: RegistroAlumno, name: 'RegistroAlumno'}),
