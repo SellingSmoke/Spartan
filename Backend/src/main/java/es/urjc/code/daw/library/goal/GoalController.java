c5859e3faa865214f47058bcdcc3e3b2a027d4c9
Sergio <sergioperezp1995@gmail.com>
2016-05-23 16:51:01 +0200

    Cambios Backend

--- Backend/src/main/java/es/urjc/code/daw/library/goal/GoalController.java
+++ Backend/src/main/java/es/urjc/code/daw/library/goal/GoalController.java
@@ -1,5 +1,6 @@
 package es.urjc.code.daw.library.goal;
 
+import es.urjc.code.daw.library.user.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,22 +8,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.urjc.code.daw.library.user.UserComponent;
+import es.urjc.code.daw.library.user.UserRepository;
 @RestController
 @RequestMapping("/goals")
 public class GoalController {
 	
 	@Autowired
 	private GoalRepository goalRepository;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private UserComponent user;
 	//private UserRepository userRepository;
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
 	public ResponseEntity<Goal> newGoal(@RequestBody Goal goal) {
-		if(goalRepository.findOne(goal.getId()) == null){
-			goalRepository.save(goal);
+			User userLogado =user.getLoggedUser();
+			userLogado.getGoals().add(goal);
+			userRepository.save(userLogado);
 			return new ResponseEntity<>(goal, HttpStatus.CREATED);
-		}else{
-			return new ResponseEntity<>(HttpStatus.CONFLICT);
-		}
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
